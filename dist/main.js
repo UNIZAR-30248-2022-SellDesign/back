c7c5e8d7ce72f1901835b110f5400643df6146e9
Gelpa99 <759406@unizar.es>
2022-10-17 13:25:54 +0200

    swagger setup

--- dist/main.js
+++ dist/main.js
@@ -4,6 +4,7 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const session = require("express-session");
 const passport = require("passport");
+const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
@@ -14,6 +15,14 @@ async function bootstrap() {
     }));
     app.use(passport.initialize());
     app.use(passport.session());
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('SellDesign API')
+        .setDescription('This document contains all the API calls for the SellDesign webSite')
+        .setVersion('1.0')
+        .addTag('SellDesign')
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api', app, document);
     await app.listen(3000);
 }
 bootstrap();
