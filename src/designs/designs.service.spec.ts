9345a81725710f351f57b996ebd6969464bdf85f
Gelpa99 <759406@unizar.es>
2022-11-09 16:28:04 +0100

    some products & designs test coverage

--- src/designs/designs.service.spec.ts
+++ src/designs/designs.service.spec.ts
@@ -3,6 +3,28 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { DesignSchema } from './designs.model';
 import { DesignsService } from './designs.service';
 
+const design = {
+  _id: '12345678',
+  name: 'testname',
+  designer: 'testdesigner',
+  image: 'testimage',
+}
+
+class designModel {
+  constructor (private data) {}
+  save = jest.fn().mockResolvedValue(this.data);
+  static findOne = jest.fn().mockResolvedValue(design);
+  static find = jest.fn().mockImplementation(() => ({
+    skip: jest.fn().mockImplementation(() => ({
+      limit: jest.fn().mockResolvedValue([design])
+    })
+  )}));
+  static skip = jest.fn(() => designModel);
+  static limit = jest.fn(() => designModel);
+  static findOneAndUpdate = jest.fn().mockResolvedValue(design);
+  static deleteOne = jest.fn().mockResolvedValue({"n": 1, "ok": 1, "deletedCount": 1});
+}
+
 describe('DesignsService', () => {
   let service: DesignsService;
 
@@ -12,7 +34,7 @@ describe('DesignsService', () => {
         DesignsService,
         {
           provide: getModelToken('design'),
-          useValue: DesignSchema
+          useValue: designModel
         }
       ],
     }).compile();
@@ -23,4 +45,36 @@ describe('DesignsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('getDesign', () => {
+    it('should return a design by its name', async () => {
+      expect(await service.getDesign("testname")).toBe(design);
+    });
+  });
+
+  describe('new design', () => {
+    it('should create and return a new design', async () => {
+      let res = await service.newDesign("testdesigner", "testimage","testname");
+      expect (res.data.name).toBe("testname");
+    });
+  });
+
+  describe('updateDesign', () => {
+    it('should update a design', async () => {
+      expect(await service.updateDesign("12345678", "testimage","testname")).toBe(design);
+    });
+  });
+
+  describe('deleteDesign', () => {
+    it('should delete a design', async () => {
+      expect(await service.deleteDesign("12345678", "testdesigner")).toBe(true);
+    });
+  });
+
+  describe('getUserDesigns', () => {
+    it('should return every design from a user', async () => {
+      expect(await service.getUserDesigns("testdesigner", "1")).toStrictEqual([design]);
+    });
+  });
+
 });
--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -6,6 +6,38 @@ import { ProductsService } from './products.service';
 import { Product } from './products.model';
 import { FavSchema } from './fav.model';
 
+const product = {
+  _id: '12345678',
+  price: 'testprice',
+  design: 'testdesign',
+  image: 'testimage',
+  type: 'testtype',
+  description: 'testdescription',
+  seller: 'testseller'
+}
+
+class productModel {
+  constructor (private data) {}
+  save = jest.fn().mockResolvedValue(this.data);
+  static findOne = jest.fn().mockImplementation(() => ({
+    populate: jest.fn().mockResolvedValue(product)
+  }));
+  static find = jest.fn().mockImplementation(() => ({
+    populate: jest.fn().mockResolvedValue([product])
+  }));
+  static findOneAndUpdate = jest.fn().mockResolvedValue(product);
+}
+
+class favModel {
+  constructor (private data) {}
+  save = jest.fn().mockResolvedValue(this.data);
+  static findOne = jest.fn().mockImplementation(() => ({
+    populate: jest.fn().mockResolvedValue(product)
+  }));
+  static find = jest.fn().mockResolvedValue([product]);
+  static findOneAndUpdate = jest.fn().mockResolvedValue(product);
+}
+
 describe('ProductsService', () => {
   let service: ProductsService;
   beforeEach(async () => {
@@ -14,11 +46,11 @@ describe('ProductsService', () => {
         ProductsService,
         {
           provide: getModelToken('product'),
-          useValue: ProductSchema
+          useValue: productModel
         },
         {
           provide: getModelToken('fav'),
-          useValue: FavSchema
+          useValue: favModel
         }
       ],
     }).compile();
@@ -28,6 +60,94 @@ describe('ProductsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getProductbyId', () => {
+    it('should return a product by its id', async () => {
+      expect(await service.getProductByID("12345678")).toBe(product);
+    });
+  });
+
+  describe('getProductByDesign', () => {
+    it('should return a product by its design', async () => {
+      expect(await service.getProductByDesign("testdesign")).toStrictEqual([product]);
+    });
+  });
+
+  describe('getHomeProducts', () => {
+    it('should return a list with products without filters', async () => {
+      productModel.find = jest.fn().mockImplementationOnce(() => ({
+        skip: jest.fn().mockImplementationOnce(() => ({
+          limit: jest.fn().mockImplementationOnce(() => ({
+            populate: jest.fn().mockResolvedValue([product])
+          }))
+        })
+      )}));
+      expect(await service.getHomeProducts(1)).toStrictEqual([product]);
+    });
+  });
+
+  describe('getHomeProductsByPrice', () => {
+    it('should return a list with products listed by price', async () => {
+      productModel.find = jest.fn().mockImplementation(() => ({
+        populate: jest.fn().mockResolvedValue([product])
+      }));
+      expect(await service.getHomeProductsByPrice("1","30")).toStrictEqual([product]);
+    });
+  });
+
+  describe('getHomeProductsByType', () => {
+    it('should return a list with products listed by type ', async () => {
+      expect(await service.getHomeProductsByType("1")).toStrictEqual([product]);
+    });
+  });
+
+  describe('getHomeProductsByPrice_Type', () => {
+    it('should return a list with products listed by type and price ', async () => {
+      expect(await service.getHomeProductsByPrice_Type("1", "30", "1")).toStrictEqual([product]);
+    });
+  });
+
+  describe('getUserFavProducts', () => {
+    it('should return a list with products faved by the user ', async () => {
+      expect(await service.getUserFavProducts("1", "1")).toStrictEqual([product]);
+    });
+  });
+
+  describe('getUserProducts', () => {
+    it('should return a list with products posted by a user ', async () => {
+      productModel.find.mockImplementationOnce(populate => [product]);
+      expect(await service.getUserProducts("12345678", "1")).toStrictEqual([product]);
+    });
+  });
+
+  describe('searchProducts', () => {
+    it('should return a list with products according to a name', async () => {
+      productModel.find = jest.fn().mockImplementationOnce(() => ({
+        skip: jest.fn().mockImplementationOnce(() => ({
+          limit: jest.fn().mockImplementationOnce(() => ({
+            populate: jest.fn().mockResolvedValue([product])
+          }))
+        })
+      )}));
+      expect(await service.searchProducts("testproduct", "1")).toStrictEqual([product]);
+    });
+  });
+
+  describe('searchProductsByPrice', () => {
+    it('should return a list with products according to a price and a name', async () => {
+      productModel.find = jest.fn().mockImplementationOnce(() => ({
+            populate: jest.fn().mockResolvedValue([product])
+      }));
+      expect(await service.searchProductsByPrice("testproduct", "1","30")).toStrictEqual([product]);
+    });
+  });
+
+  describe('addProduct', () => {
+    it('should create and return a new product ', async () => {
+      let res = await service.addProduct("testprice", "testdesign", "testimage","testtype","testdescription","testseller");
+      expect(res.data.description).toBe(product.description);
+    });
   });
 
 });
